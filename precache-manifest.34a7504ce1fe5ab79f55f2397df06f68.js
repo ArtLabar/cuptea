0fea400a8e48800c83beec30df25dd03c8fec026
a.labar <artem.labar@gmail.com>
2021-02-04 12:45:18 +0300

    start project

---
+++ precache-manifest.34a7504ce1fe5ab79f55f2397df06f68.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e75c16036942f2c11cedc478985ef64",
+    "url": "/cuptea/index.html"
+  },
+  {
+    "revision": "e5675c0cc0a438da02c0",
+    "url": "/cuptea/static/css/2.636c4284.chunk.css"
+  },
+  {
+    "revision": "ea3b2c8cddec4fa1e122",
+    "url": "/cuptea/static/css/main.05acedf7.chunk.css"
+  },
+  {
+    "revision": "e5675c0cc0a438da02c0",
+    "url": "/cuptea/static/js/2.bde6229f.chunk.js"
+  },
+  {
+    "revision": "ea3b2c8cddec4fa1e122",
+    "url": "/cuptea/static/js/main.32cdc9fe.chunk.js"
+  },
+  {
+    "revision": "da118cf5b4a08b491652",
+    "url": "/cuptea/static/js/runtime-main.1258d37a.js"
+  },
+  {
+    "revision": "e7e28eec30b251d5fdf7d1fe1c86212b",
+    "url": "/cuptea/static/media/basket.e7e28eec.svg"
+  },
+  {
+    "revision": "b2390a94c1a3538d58ce538b3ec1ecc0",
+    "url": "/cuptea/static/media/close.b2390a94.svg"
+  },
+  {
+    "revision": "4752ba82a50cbc8217a24beb817f24bc",
+    "url": "/cuptea/static/media/delete.4752ba82.svg"
+  },
+  {
+    "revision": "5eeacb13d4a37df18c0c48eab2113550",
+    "url": "/cuptea/static/media/facebook.5eeacb13.svg"
+  },
+  {
+    "revision": "186d414165ddf02dde78f6f29b6a9ff7",
+    "url": "/cuptea/static/media/instagram.186d4141.svg"
+  },
+  {
+    "revision": "e246ed5e63e280bc872029a1a358cf29",
+    "url": "/cuptea/static/media/menu.e246ed5e.svg"
+  },
+  {
+    "revision": "9b5b8f3dad68af59d5ce2f90acb76233",
+    "url": "/cuptea/static/media/next.9b5b8f3d.svg"
+  },
+  {
+    "revision": "57e02c4474dd23bec1599767ff629872",
+    "url": "/cuptea/static/media/prev.57e02c44.svg"
+  },
+  {
+    "revision": "9d90dcd796d99322ca710a677347d120",
+    "url": "/cuptea/static/media/twitter.9d90dcd7.svg"
+  },
+  {
+    "revision": "24139944245f8032daf268c222f62c3e",
+    "url": "/cuptea/static/media/youtube.24139944.svg"
+  }
+]);
